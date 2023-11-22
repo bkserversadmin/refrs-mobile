8499d263226e599b51f0c35f96f3fde49b96f231
Enrique Sarmiento <jeisonsarmiento@correo.unimet.edu.ve>
2023-11-22 15:50:56 -0400

    notification header component created

--- components/ui/Header.js
+++ components/ui/Header.js
@@ -2,6 +2,7 @@ import { withTheme, Link, Icon, Touchable, IconButton } from "@draftbit/ui";
 import { Text, View, useWindowDimensions } from "react-native";
 import * as StyleSheet from "../../utils/StyleSheet";
 import * as GlobalStyles from "../../GlobalStyles.js";
+import NotificationHeader from "../notifications/notificationHeader.js";
 
 const Header = (props) => {
 	const { theme, navigation, name, canGoBack = false } = props;
@@ -53,78 +54,7 @@ const Header = (props) => {
 				{name}
 			</Text>
 
-			<Touchable
-				onPress={() => {
-					try {
-						navigation.navigate("NotificationsScreen");
-					} catch (err) {
-						console.error(err);
-					}
-				}}
-			>
-				{/* notification container */}
-				<View
-					style={StyleSheet.applyWidth(
-						{
-							alignContent: "center",
-							alignItems: "center",
-							alignSelf: "center",
-							backgroundColor: "rgb(255, 255, 255)",
-							borderRadius: 100,
-							height: 40,
-							justifyContent: "center",
-							width: 40,
-						},
-						dimensions.width
-					)}
-				>
-					<Icon
-						color={theme.colors["Primary/Yellow800"]}
-						name={"Ionicons/notifications"}
-						size={24}
-						style={StyleSheet.applyWidth(
-							{ backgroundColor: "rgba(0, 0, 0, 0)" },
-							dimensions.width
-						)}
-					/>
-					{/* Notification Quantity */}
-					<View
-						style={StyleSheet.applyWidth(
-							{
-								alignContent: "center",
-								alignItems: "center",
-								alignSelf: "center",
-								backgroundColor: theme.colors["System/Error500"],
-								borderColor: theme.colors["Primary/Yellow100"],
-								borderRadius: 100,
-								borderWidth: 2,
-								height: 14,
-								justifyContent: "center",
-								position: "absolute",
-								right: 8,
-								top: 6,
-								width: 14,
-							},
-							dimensions.width
-						)}
-					>
-						<Text
-							accessible={true}
-							allowFontScaling={true}
-							style={StyleSheet.applyWidth(
-								StyleSheet.compose(GlobalStyles.TextStyles(theme)["Text"], {
-									color: "rgb(255, 255, 255)",
-									fontFamily: "Inter_700Bold",
-									fontSize: 9,
-								}),
-								dimensions.width
-							)}
-						>
-							{"2"}
-						</Text>
-					</View>
-				</View>
-			</Touchable>
+			<NotificationHeader navigation={navigation} />
 		</View>
 	);
 };
