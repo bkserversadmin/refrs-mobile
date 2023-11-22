84e07c0a72b3dae0cdf42d68ab127f9c91939c16
Enrique Sarmiento <jeisonsarmiento@correo.unimet.edu.ve>
2023-11-22 11:37:38 -0400

    other profile components created

---
+++ components/profile/forms/AssignorProfileForm.js
@@ -0,0 +1,95 @@
+import { withTheme, Button } from "@draftbit/ui";
+import { Text, View, useWindowDimensions } from "react-native";
+import * as GlobalStyles from "../../../GlobalStyles.js";
+import * as StyleSheet from "../../../utils/StyleSheet.js";
+import InputField from "../../ui/InputField.js";
+import InputPhone from "../../ui/InputPhone";
+import SelectField from "../../ui/SelectField.js";
+import TextArea from "../../ui/TextArea.js";
+
+export const AssignorProfileForm = (props) => {
+	const { theme } = props;
+	const dimensions = useWindowDimensions();
+
+	return (
+		<>
+			<View>
+				<Text
+					accessible={true}
+					allowFontScaling={true}
+					style={StyleSheet.applyWidth(
+						StyleSheet.compose(GlobalStyles.TextStyles(theme)["Text"], {
+							color: theme.colors["Primary/Yellow900"],
+							fontFamily: "Inter_400Regular",
+							fontSize: 16,
+							letterSpacing: 0.15,
+							marginBottom: 20,
+						}),
+						dimensions.width
+					)}
+				>
+					Detailed information
+				</Text>
+
+				<InputField placeholder="First Name" label="first name" theme={theme} />
+				<InputField placeholder="Last Name" label="Last name" theme={theme} />
+				<SelectField
+					options={[]}
+					placeholder={"Select an option"}
+					label="Sports"
+					theme={theme}
+				/>
+				<SelectField
+					options={[]}
+					placeholder={"Select an option"}
+					label="State"
+					theme={theme}
+				/>
+				<SelectField
+					options={[]}
+					placeholder={"Select an option"}
+					label="City"
+					theme={theme}
+				/>
+
+				<InputPhone
+					label="Contact Number"
+					placeholder="xxx-xxxx"
+					onChange={(val) => console.log("el val phone ===>", val)}
+					theme={theme}
+				/>
+				<InputField placeholder="email" label="email" theme={theme} />
+
+				<TextArea
+					theme={theme}
+					label="About the Assigning company"
+					onChange={(val) => console.log("el val", val)}
+				/>
+
+				<Button
+					onPress={() => {
+						const handler = async () => {
+							console.log("Primary button ON_PRESS Start");
+						};
+						handler();
+					}}
+					style={StyleSheet.applyWidth(
+						StyleSheet.compose(GlobalStyles.ButtonStyles(theme)["Button"], {
+							backgroundColor: "rgb(248, 211, 71)",
+							borderRadius: 100,
+							color: "rgb(39, 31, 1)",
+							fontFamily: "Inter_500Medium",
+							fontSize: 16,
+							letterSpacing: 1.25,
+							width: "100%",
+						}),
+						dimensions.width
+					)}
+					title={"Save changes"}
+				/>
+			</View>
+		</>
+	);
+};
+
+export default withTheme(AssignorProfileForm);
